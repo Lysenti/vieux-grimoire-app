4d26f863f872302ca7c6f9dbb6f1d52fe28ed1dc
Lysenti <eren926@gmail.com>
2024-09-12 14:28:34 +0200

    "Optimisation des images"

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,108 +1,153 @@
 import Book from '../models/book.js';
+import multer from 'multer';
 
 
+// Configure multer pour l'upload en mémoire
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+const upload = multer({ storage });
 
 // Fonction pour générer l'URL complète de l'image
 const saveImageUrl = (imagePath) => {
-  const backendUrl = process.env.BACKEND_URL || 'http://localhost:4000'; 
-  const imageUrl = `${backendUrl}/${imagePath}`; // Génération de l'URL complète
+  const backendUrl = process.env.BACKEND_URL || 'http://localhost:4000';
+  const imageUrl = `${backendUrl}/${imagePath.replace(/\\/g, '/')}`; 
   return imageUrl;
 };
 
+// Middleware pour l'upload d'image
+export const uploadImage = upload.single('image');
 
-// Créer un livre avec upload d'image
+// Créer un nouveau livre
 export const createBook = async (req, res) => {
   try {
-    // Si une image est envoyée, on génère l'URL complète de l'image
-    let imageUrl;
-    if (req.file) {
-      imageUrl = saveImageUrl(`uploads/${req.file.filename}`);
+    // Extraire le champ 'book' du corps de la requête et le parser
+    const { book } = req.body;
+    const parsedBook = JSON.parse(book);
+
+    // Vérifier que le fichier a été reçu et qu'il est valide
+    if (!req.file || !req.file.path) {
+      return res.status(400).json({ message: 'Image non reçue ou invalide.' });
     }
 
-    const reqBook = JSON.parse(req.body.book);
-    const rating = reqBook.ratings ? reqBook.ratings[0] : undefined;
-    if (rating) {
-      rating.userId = req.user._id.toString();  // Convertit en chaîne de caractères
-    }
+     // Générer l'URL complète de l'image
+     const imageUrl = saveImageUrl(`uploads/${req.file.filename}`);
 
-    // Créer un nouveau livre avec les données envoyées et l'URL de l'image (si présente)
-    const book = new Book({
-      userId: req.user._id.toString(),  // Convertit en chaîne de caractères
-      title: reqBook.title,
-      author: reqBook.author,
-      imageUrl: imageUrl || '',  
-      year: reqBook.year, 
-      genre: reqBook.genre,  
-      ratings: rating ? [rating] : [],  
-      averageRating: rating ? rating.grade : 0 ,  
-     });
 
-    // Sauvegarder le livre dans la base de données
-    await book.save();
-    res.status(201).json(book);
+    // Créer un nouvel objet livre avec les données reçues
+    const newBook = new Book({
+      ...parsedBook,
+      imageUrl,
+    });
+
+    // Enregistrer le nouveau livre dans la base de données
+    await newBook.save();
+
+    // Retourner le livre créé avec succès
+    res.status(201).json(newBook);
   } catch (error) {
-    res.status(500).json({ error: 'Erreur lors de la création du livre.' });
+    console.error('Erreur lors de la création du livre:', error);
+    res.status(500).json({ message: error.message });
   }
 };
-
-
 
 // Obtenir tous les livres
 export const getAllBooks = async (req, res) => {
   try {
-    const books = await Book.find().populate('userId', 'name email');
-
-    // Convertir userId en chaîne de caractères dans chaque livre
-    const booksWithStringUserId = books.map(book => {
-      // Convertit userId en chaîne si c'est un objet
-      if (book.userId && typeof book.userId === 'object') {
-        book.userId = book.userId._id.toString();
-      }
-      return book;
-    });
-
-    res.status(200).send(booksWithStringUserId);
-  } catch (err) {
-    console.error('Erreur lors de la récupération des livres:', err.message); 
-    res.status(500).send({ error: 'Erreur serveur lors de la récupération des livres.' });
+    const books = await Book.find({});
+    res.status(200).json(books);
+  } catch (error) {
+    console.error('Erreur lors de la récupération des livres:', error);
+    res.status(500).json({ message: error.message });
   }
 };
 
 // Obtenir un livre par ID
 export const getBookById = async (req, res) => {
   try {
-    const book = await Book.findById(req.params.id).populate('userId', 'name email');
-
-    if (!book) {
-      return res.status(404).send({ error: 'Book not found' });
+    const book = await Book.findById(req.params.id);
+    if (book) {
+      res.status(200).json(book);
+    } else {
+      res.status(404).json({ message: 'Livre non trouvé' });
     }
-
-    // Convertit userId en chaîne si c'est un objet
-    if (book.userId && typeof book.userId === 'object') {
-      book.userId = book.userId._id.toString();
-    }
-
-    res.status(200).send(book);
-  } catch (err) {
-    console.error('Erreur lors de la récupération du livre par ID:', err.message); 
-    res.status(500).send({ error: 'Erreur serveur lors de la récupération du livre.' });
+  } catch (error) {
+    console.error('Erreur lors de la récupération du livre:', error);
+    res.status(500).json({ message: error.message });
   }
 };
 
-// Mettre à jour un livre par ID
 export const updateBookById = async (req, res) => {
   try {
-    const bookData = JSON.parse(req.body.book);
-    if (req.file) {
-      bookData.imageUrl = `/uploads/${req.file.filename}`;
-    }
-    const book = await Book.findByIdAndUpdate(req.params.id, bookData, { new: true });
+    const { book } = req.body;
+
+
     if (!book) {
-      return res.status(404).send({ error: 'Book not found' });
+      return res.status(400).json({ message: 'Les données du livre sont manquantes ou invalides.' });
     }
-    res.status(200).send(book);
-  } catch (err) {
-    res.status(500).send({ error: err.message });
+
+    const parsedBook = JSON.parse(book);
+
+
+    // Recherche du livre par ID pour mettre à jour ses propriétés
+    const bookToUpdate = await Book.findById(req.params.id);
+
+    if (!bookToUpdate) {
+      return res.status(404).json({ message: 'Livre non trouvé' });
+    }
+
+     // Vérifier que l'utilisateur est authentifié
+     if (!req.user || !req.user._id) {
+      return res.status(401).send({ error: 'User is not authenticated.' });
+    }
+
+    // Vérifiez si l'utilisateur est le créateur du livre
+    const isCreator = bookToUpdate.userId.toString() === req.user._id.toString();
+
+    // Si l'utilisateur est le créateur, permettre la mise à jour complète
+    if (isCreator) {
+      // Vérifiez s'il y a un fichier image et générez une nouvelle URL d'image si nécessaire
+      let imageUrl = parsedBook.imageUrl;
+      if (req.file) {
+        imageUrl = saveImageUrl(`uploads/${req.file.filename}`);
+      }
+
+      // Mettre à jour les propriétés du livre
+      bookToUpdate.title = parsedBook.title || bookToUpdate.title;
+      bookToUpdate.author = parsedBook.author || bookToUpdate.author;
+      bookToUpdate.year = parsedBook.year || bookToUpdate.year;
+      bookToUpdate.genre = parsedBook.genre || bookToUpdate.genre;
+      bookToUpdate.imageUrl = imageUrl;
+    }
+
+    // Mise à jour du rating (note) pour tous les utilisateurs
+    if (parsedBook.rating) {
+      const existingRating = bookToUpdate.ratings.find(
+        (rating) => rating.userId.toString() === req.auth.userId.toString()
+      );
+
+      if (existingRating) {
+        existingRating.grade = parsedBook.rating;
+      } else {
+        bookToUpdate.ratings.push({ userId: req.auth.userId, grade: parsedBook.rating });
+      }
+
+      // Recalculer la note moyenne
+      bookToUpdate.averageRating = bookToUpdate.ratings.reduce((acc, curr) => acc + curr.grade, 0) / bookToUpdate.ratings.length;
+    }
+
+    // Sauvegarder le livre mis à jour
+    const updatedBook = await bookToUpdate.save();
+
+    res.status(200).json(updatedBook);
+  } catch (error) {
+    console.error('Erreur lors de la mise à jour du livre:', error);
+    res.status(500).json({ message: error.message });
   }
 };
 
@@ -110,23 +155,22 @@ export const updateBookById = async (req, res) => {
 export const deleteBookById = async (req, res) => {
   try {
     const book = await Book.findByIdAndDelete(req.params.id);
-    if (!book) {
-      return res.status(404).send({ error: 'Book not found' });
+    if (book) {
+      res.status(200).json({ message: 'Livre supprimé avec succès' });
+    } else {
+      res.status(404).json({ message: 'Livre non trouvé' });
     }
-    res.status(200).send({ message: 'Book deleted successfully' });
-  } catch (err) {
-    res.status(500).send({ error: err.message });
+  } catch (error) {
+    console.error('Erreur lors de la suppression du livre:', error);
+    res.status(500).json({ message: error.message });
   }
 };
 
-// Noter un livre
+// Ajouter une note à un livre
 export const rateBook = async (req, res) => {
-
   const { rating } = req.body;
 
   try {
-    const { grade } = req.body;
-
     if (!rating) {
       return res.status(400).send({ error: 'Rating is required' });
     }
@@ -152,7 +196,7 @@ export const rateBook = async (req, res) => {
     }
 
     // Ajoute la nouvelle notation
-    book.ratings.push({ userId: req.user._id, grade: rating }); // Utilise `rating` pour définir `grade`
+    book.ratings.push({ userId: req.user._id, grade: rating });
 
     // Recalcule la moyenne des notations
     book.averageRating = book.ratings.reduce((acc, rating) => acc + rating.grade, 0) / book.ratings.length;
@@ -168,22 +212,13 @@ export const rateBook = async (req, res) => {
 };
 
 
-
+// Obtenir les livres les mieux notés
 export const getBestRatedBooks = async (req, res) => {
   try {
-    const books = await Book.find().sort({ averageRating: -1 }).limit(5).populate('userId', 'name email');
-
-    // Convertir userId en chaîne de caractères dans chaque livre
-    const booksWithStringUserId = books.map(book => {
-      if (book.userId && typeof book.userId === 'object') {
-        book.userId = book.userId._id.toString();
-      }
-      return book;
-    });
-
-    res.status(200).send(booksWithStringUserId);
-  } catch (err) {
-    console.error('Erreur lors de la récupération des livres les mieux notés:', err.message);
-    res.status(500).send({ error: 'Erreur serveur lors de la récupération des livres les mieux notés.' });
+    const books = await Book.find({}).sort({ averageRating: -1 }).limit(5);
+    res.status(200).json(books);
+  } catch (error) {
+    console.error('Erreur lors de la récupération des livres les mieux notés:', error);
+    res.status(500).json({ message: error.message });
   }
 };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import multer from 'multer'; // Importer multer pour la gestion des fichiers
 import connectDB from './config/db.js'; 
 import bookRoutes from './routes/bookRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -13,21 +14,59 @@ const PORT = process.env.PORT || 4000;
 // Connect to MongoDB
 connectDB();
 
-// Middleware
+// Middleware pour JSON
 app.use(express.json());
 
-//Activation de CORS
+// Activation de CORS
 app.use(cors({
   origin: '*', // Accepter toutes les origines
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Accepter toutes les méthodes HTTP
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.use('/uploads', express.static('uploads')); // Servir les fichiers statiques du répertoire 'uploads'
+// Configuration de multer pour la gestion des fichiers
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // Répertoire où les fichiers seront sauvegardés
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname); // Nomme le fichier de manière unique
+  }
+});
+
+// Filtrer les types de fichiers acceptés
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(new Error('Format de fichier non pris en charge'), false);
+  }
+};
+
+// Créer l'instance multer
+const upload = multer({ 
+  storage: storage,
+  fileFilter: fileFilter
+});
+
+// Servir les fichiers statiques du répertoire 'uploads'
+app.use('/uploads', express.static('uploads'));
 
 // Routes
 app.use('/api/books', bookRoutes);
 app.use('/api/auth', userRoutes);
+
+// Route pour l'upload de fichiers
+app.post('/api/upload', upload.single('image'), (req, res) => {
+  console.log('Corps de la requête:', req.body);
+  console.log('Fichier reçu:', req.file);
+
+  if (!req.file) {
+    return res.status(400).send('Aucun fichier reçu');
+  }
+
+  res.send('Fichier uploadé avec succès');
+});
 
 app.get('/', (req, res) => {
   res.send('Hello, Vieux grimoire !');
